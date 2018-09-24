185d783109deedaa71824ec7580cca87d3db9178
nata <nata.mozhaueva@gmail.com>
2018-09-24 13:02:17 +0300

    res

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+'use strict'
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	let counter = 0;
+	for (let i = 0; i < preferences.length; i++){
+		let a = preferences[i];
+		let b = preferences[a-1];
+		let c = preferences[b-1];
+		if (a == preferences[a-1] || b == preferences[b-1] || c == preferences[c-1]) continue;
+		if (c == i+1) {
+			counter++;
+		}
+	}
+	return counter / 3;
 };
+
